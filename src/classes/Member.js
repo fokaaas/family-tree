e3c224a068b1a623357ead8790640ce03c9364e9
Stanislav Basarab <stanislavbasarab083@gmail.com>
2023-03-22 16:03:34 +0200

    Add simple methods for Member: rename, addEvent, connect

--- src/classes/Member.js
+++ src/classes/Member.js
@@ -7,9 +7,35 @@ class Member {
     this.id = generateID();
     this.name = {
       first: firstName,
-      last: lastName,
+      last: lastName
     };
-    this.events = { birth };
+    this.events = { [birth]: ['Birth'] };
     this.connections = {};
+  }
+
+  rename(fullName) {
+    [ this.name.first, this.name.last ] = fullName.split(' ');
+  }
+
+  addEvent(year, description) {
+    if (!Object.hasOwn(this.events, year)) {
+      const box = [description];
+      this.events[year] = box;
+      return;
+    }
+    this.events[year].push(description);
+  }
+
+  connect(type, fullName, members) {
+    const [ firstName, lastName ] = fullName.split(' ');
+    for (const member of members) {
+      const { first, last } = member.name;
+      if (firstName === first && lastName === last) {
+        const { id } = member;
+        this.connections[type] = id;
+        return;
+      }
+    }
+    throw new Error('The specified person was not found.');
   }
 }
