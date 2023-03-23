e0e962a338486d367809a7d435fbaa8e5988b7f5
Stanislav Basarab <stanislavbasarab083@gmail.com>
2023-03-23 16:47:54 +0200

    Implement add and remove member methods for Tree

--- src/classes/Tree.js
+++ src/classes/Tree.js
@@ -8,4 +8,23 @@ class Tree {
     this.root = root;
     this.members = [ root ];
   }
+
+  addMember(fullName, birth) {
+    const names = fullName.split(' ');
+    const member = new Member(...names, birth);
+    this.members.push(member);
+  }
+
+  removeMember(fullName) {
+    const [first, last] = fullName.split(' ');
+    const { members } = this;
+    for (let i = 0; i < members.length; i++) {
+      const member = members[i];
+      if (member.name.first === first && member.name.last === last) {
+        this.members.splice(i, 1);
+        return;
+      }
+    }
+    throw new Error('The specified person was not found.');
+  }
 }
