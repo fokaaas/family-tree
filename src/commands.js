d4ba61d79359a2ca3b23a7280f44397a9cdfa28c
Stanislav Basarab <stanislavbasarab083@gmail.com>
2023-03-25 23:40:11 +0200

    Implement common command `create`

--- src/commands.js
+++ src/commands.js
@@ -10,3 +10,14 @@ const rl = readlinePromises.createInterface({
 });
 
 const state = {};
+
+const commands = {
+  common: {
+    async create() {
+      const name = await rl.question('Enter a name for your tree: ');
+      const rootName = await rl.question('Enter the name of root person: ');
+      const rootBirth = await rl.question('Enter the root\'s year of birth: ');
+      state.tree = Tree.create(name, rootName, rootBirth);
+    }
+  }
+};
