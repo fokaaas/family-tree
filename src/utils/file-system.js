125b73e18f7add105f356ae90d34ddb41f666744
Stanislav Basarab <stanislavbasarab083@gmail.com>
2023-04-08 22:00:13 +0300

    Add collection of path parts

---
+++ src/utils/file-system.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const fs = require('fs/promises');
+const path = require('path');
+
+const PATH = {
+  about: '../../docs/about.txt',
+  common: '../../docs/common.txt',
+  tree: '../../docs/tree.txt',
+  member: '../../docs/member.txt',
+};
