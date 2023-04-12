4f0f35954c0094c61f533575faf3f9a4c63a0dd0
Stanislav Basarab <stanislavbasarab083@gmail.com>
2023-04-12 17:11:41 +0300

    Add `saved` rule

--- src/utils/file-system.js
+++ src/utils/file-system.js
@@ -1,7 +1,9 @@
 'use strict';
 
-const fs = require('fs/promises');
-const path = require('path');
+const fs = require('node:fs');
+const fsPromises = require('node:fs/promises');
+const path = require('node:path');
+const { Tree } = require('../classes/Tree.js');
 
 const PATH = {
   about: '../../docs/about.txt',
@@ -10,8 +12,26 @@ const PATH = {
   member: '../../docs/member.txt',
 };
 
-const logInfo = (name) => fs.readFile(path.resolve(__dirname, PATH[name]), 'utf-8')
+const fileState = {
+  directory: '../../saved',
+  current: '',
+};
+
+const logInfo = (name) => fsPromises
+  .readFile(path.resolve(__dirname, PATH[name]), 'utf-8')
   .then((text) => console.log(text))
   .catch((err) => console.log(err.message));
+
+const serialize = async (name, target) => {
+  const fileName = `${name}.json`;
+  const content = JSON.stringify(target);
+  await fsPromises.writeFile('../../saved/' + fileName, content);
+};
+
+const tree = Tree.create('Basarab Family', 'Stanislav Basarab', 2005);
+tree.addMember('Anastasia Basarab', 2009);
+tree.addMember('Anatoliy Basarab', 1978);
+serialize('my-tree', tree);
+
 
 module.exports = { logInfo };
