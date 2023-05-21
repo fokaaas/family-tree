2f8f192f5ee289b9d37ac1b8408d64d24438570f
Stanislav Basarab <stanislavbasarab083@gmail.com>
2023-05-21 17:06:30 +0300

    feature: implemented tree serialization
    - implemented serialize function
    - added `JSON.stringify()` `replacer`
    - removed unused code

--- src/utils/file-system.js
+++ src/utils/file-system.js
@@ -3,50 +3,35 @@
 const fs = require('node:fs/promises');
 const path = require('node:path');
 
-const PATH = {
+const PATHS = {
   about: '../../docs/about.txt',
   common: '../../docs/common.txt',
   tree: '../../docs/tree.txt',
   member: '../../docs/member.txt',
+  saved: '../../saved/',
 };
 
 const logInfo = (name) => fs
-  .readFile(path.resolve(__dirname, PATH[name]), 'utf-8')
+  .readFile(path.resolve(__dirname, PATHS[name]), 'utf-8')
   .then((text) => console.log(text))
   .catch((err) => console.log(err.message));
 
-const parseMember = (member) => {
-  const name = member.name;
-  return `${name.first} ${name.last}`;
-};
-
-const parseRelations = (member) => {
-  const relations = member.relations;
-  for (const type in relations) {
-    const relatives = relations[type];
-    relations[type] = relatives.map((mem) => parseMember(mem));
+const replacer = (key, value) => {
+  if (key === 'relations') {
+    const members = {};
+    for (const type in value) {
+      const relatives = value[type];
+      members[type] = relatives.map((mem) => mem.fullName());
+    }
+    return members;
   }
-  return relations;
+  return value;
 };
 
-const toUncircular = (target) => {
-  const members = target.members;
-  target.members = members.map((mem) => parseRelations(mem));
-  return target;
-};
-
-const serialize = async (name, target) => {
+const serialize = async (name, tree) => {
   const fileName = `${name}.json`;
-  const tree = toUncircular(target);
-  const content = JSON.stringify(tree);
-  await fs.writeFile('../../saved/' + fileName, content);
+  const content = JSON.stringify(tree, replacer);
+  await fs.writeFile(path.resolve(PATHS.saved, fileName), content);
 };
 
-const deserialize = async (name) => {
-  const fileName = name.includes('.') ? name : `${name}.json`;
-  const content = await fs.readFile('../../saved/' + fileName, 'utf-8');
-  const tree = JSON.parse(content);
-  return tree;
-};
-
-module.exports = { logInfo, serialize, deserialize };
+module.exports = { logInfo, serialize };
